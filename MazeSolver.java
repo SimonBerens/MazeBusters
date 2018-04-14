da1c373739855443b77cfd6fbf655d9f79ef282f
SpO0ky <simontech101@gmail.com>
2018-04-13 21:14:57 -0400

    made solver compatible with the refactored Maze class, cleaned up some code

--- MazeSolver.java
+++ MazeSolver.java
@@ -5,27 +5,33 @@
 
 public class MazeSolver {
 
-    static Displayer displayer = new Displayer(1);
+    /**
+     * Finds out if the maze is solvable by trying to move in
+     * all directions if the explorer is on a valid Tile
+     * @param maze the maze to solve
+     * @return the solvability of the maze
+     */
 
     public static boolean solve(Maze maze) {
-        displayer.atTopOfWindow(maze.toString());
-        if (maze.explorerIsOnA() == Maze.TREASURE) return true; // TODO: 4/11/2018 make @explorerIsOnA take a parameter
-                                                                        // ??
-        if (maze.explorerIsOnA() != Maze.STEPPING_STONE) return false;
-        // TODO: 4/12/2018 add check for if explorer is outside of maze (explorer == null) URGENT
-            // ok but like why don't we just keep calling it a wall
-        // TODO: 4/11/2018 make go return a maze
-        // TODO: 4/11/2018 make dropA return a maze 
-        Maze northMaze = new Maze(maze);
-        Maze southMaze = new Maze(maze);
-        Maze eastMaze = new Maze(maze);
-        Maze westMaze = new Maze(maze);
-        Maze[] mazes = {northMaze, southMaze, eastMaze, westMaze,};
-        int[] directions = {Maze.NORTH, Maze.SOUTH, Maze.EAST, Maze.WEST,};
-        for(int i = 0; i < 4; i++) {
-            mazes[i].dropA(Maze.WALL);
-            mazes[i].go(directions[i]);
+
+        // base cases
+        if (maze.explorerIsOnA() == Maze.Tile.TREASURE) return true;
+        if (maze.explorerIsOnA() != Maze.Tile.STEPPING_STONE) return false;
+
+        // save the maze so you can backtrack
+        Maze snapshot = new Maze(maze);
+
+        // try every direction
+        for (Maze.Direction direction:
+        Maze.Direction.values()){
+
+            // call the recursive abstraction after turning the current position
+            // into a wall and moving in a direction
+            if(solve(maze.dropA(Maze.Tile.WALL).go(direction))) return true;
+
+            // set the current maze back to the snapshot
+            maze = new Maze(snapshot);
         }
-        return solve(northMaze) || solve(southMaze) || solve(eastMaze) || solve(westMaze);
+        return false;
     }
 }
